a378000c002e2578e85332c432a16a41b7795db9
rmcc <github@cerqueira.org>
2010-10-22 17:31:33 +0100

    Z71: Initial config
    The Z71 is the "base variety" of the following devices (at least):
    
    Gigabyte G1305
    Orange Boston
    Cincinnati Bell Blaze
    Optimus Boston
    Apanda A60
    Motorola XT502
    
    and a few others I can't remember...

---
+++ AndroidProducts.mk
@@ -0,0 +1,2 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/device_z71.mk
---
+++ BoardConfig.mk
@@ -0,0 +1,41 @@
+USE_CAMERA_STUB := true
+
+# inherit from the proprietary version
+-include vendor/commtiva/z71/BoardConfigVendor.mk
+
+TARGET_NO_BOOTLOADER := true
+TARGET_BOARD_PLATFORM := unknown
+TARGET_CPU_ABI := armeabi
+TARGET_BOOTLOADER_BOARD_NAME := z71
+
+BOARD_KERNEL_CMDLINE := 
+BOARD_KERNEL_BASE := 0x12e00000
+BOARD_PAGE_SIZE := 0x00000800
+
+# fix this up by examining /proc/mtd on a running device
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00380000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00480000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x08c60000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+TARGET_PREBUILT_KERNEL := device/commtiva/z71/kernel
+
+# Below is a sample of how you can tweak the mount points using the board config.
+# This is for the Samsung Galaxy S.
+# Feel free to tweak or remove this code.
+# If you want to add/tweak a mount point, the BOARD_X_FILESYSTEM_OPTIONS are optional.
+#BOARD_DATA_DEVICE := /dev/block/mmcblk0p2
+#BOARD_DATA_FILESYSTEM := auto
+#BOARD_DATA_FILESYSTEM_OPTIONS := llw,check=no,nosuid,nodev
+#BOARD_HAS_DATADATA := true
+#BOARD_DATADATA_DEVICE := /dev/block/stl10
+#BOARD_DATADATA_FILESYSTEM := auto
+#BOARD_DATADATA_FILESYSTEM_OPTIONS := llw,check=no,nosuid,nodev
+#BOARD_SYSTEM_DEVICE := /dev/block/stl9
+#BOARD_SYSTEM_FILESYSTEM := auto
+#BOARD_SYSTEM_FILESYSTEM_OPTIONS := llw,check=no
+#BOARD_CACHE_DEVICE := /dev/block/stl11
+#BOARD_CACHE_FILESYSTEM := auto
+#BOARD_CACHE_FILESYSTEM_OPTIONS := llw,check=no,nosuid,nodev
+#BOARD_SDEXT_DEVICE := /dev/block/mmcblk1p2
