c2325eef47814f7e518189990f1f516486380e27
rmcc <github@cerqueira.org>
2010-11-04 15:15:54 +0000

    Add keyboard layout
    Fixes wrong keycode sent by trackball click

--- device_z71.mk
+++ device_z71.mk
@@ -160,7 +160,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
 	device/commtiva/z71/dhcpcd.conf:system/etc/dhcpcd/dhcpcd.conf \
 	device/commtiva/z71/vold.fstab:system/etc/vold.fstab \
-	device/commtiva/z71/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
+	device/commtiva/z71/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
+	device/commtiva/z71/7x27_kybd.kl:system/usr/keylayout/7x27_kybd.kl
 
 
 $(call inherit-product, build/target/product/full.mk)
