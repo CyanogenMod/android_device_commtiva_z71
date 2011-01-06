my4ndr0id/android_vendor_qcom_android-open

Camera: Pass Exif Altitude value in meters.
EXIF standards required the GPSAltitude tag should be specified in meters

Change-Id: I2c1e3cf17f913a466c314887f00f5bb6175a3478

libcamera2/QualcommCameraHardware.cpp
@@ -1823,13 +1823,14 @@ void QualcommCameraHardware::setGpsParameters() {
     str = NULL;
     str = mParameters.get(CameraParameters::KEY_GPS_ALTITUDE);
     if(str != NULL) {
-        int value = atoi(str);
+        double value = atof(str);
         int ref = 0;
         if(value < 0){
             ref = 1;
             value = -value;
         }
-        rat_t alt_value = {value, 1000};
+        uint32_t value_meter = value * 1000;
+        rat_t alt_value = {value_meter, 1000};
         memcpy(&altitude, &alt_value, sizeof(altitude));
         addExifTag(EXIFTAGID_GPS_ALTITUDE, EXIF_RATIONAL, 1,
                     1, (void *)&altitude);

CyanogenMod/android_device_commtiva_z71

libcamera/QualcommCameraHardware.cpp
@@ -1803,8 +1803,9 @@ void QualcommCameraHardware::setGpsParameters() {
     str = NULL;
     str = mParameters.get(CameraParameters::KEY_GPS_ALTITUDE);
     if(str != NULL) {
-        int value = atoi(str);
-        rat_t alt_value = {value, 1000};
+        double value = atoi(str);
+        uint32_t value_meter = value * 1000;
+        rat_t alt_value = {value_meter, 1000};
         memcpy(&altitude, &alt_value, sizeof(altitude));
         addExifTag(EXIFTAGID_GPS_ALTITUDE, EXIF_RATIONAL, 1,
                     1, (void *)&altitude);